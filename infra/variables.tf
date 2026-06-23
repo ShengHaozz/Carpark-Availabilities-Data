7529fb23778a37da6dc1143964e34fbb423f4dcc
ShengHaozz <shenghaoong@gmail.com>
2026-06-23 23:03:19 +0800

    Add aws terraform user creds

--- infra/variables.tf
+++ infra/variables.tf
@@ -5,3 +5,13 @@ variable "aws_region" {
 variable "bucket_name" {
   default = "shenghao-car-availability-bucket"
 }
+
+variable "access_key" {
+  type        = string
+  description = "Access Key for Terraform IAM user"
+}
+
+variable "secret_key" {
+  type        = string
+  description = "Secret Key for Terraform IAM User"
+}
