74dbad16e82433341c183bc797517fd980212a3a
ShengHaozz <shenghaoong@gmail.com>
2026-06-28 21:56:07 +0800

    Split codebase into packages

---
+++ packages/hdb_poller/src/hdb_poller/__init__.py
@@ -0,0 +1,2 @@
+def hello() -> str:
+    return "Hello from hdb-poller!"
--- src/index.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def main():
-    print("Hello World!")
-    print(f"Bucket: {os.environ["BUCKET_NAME"]}")
-
-def handler(event, context):
-    print(f"Event: {event}")
-    main()
