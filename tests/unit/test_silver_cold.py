d89336b56aebcc44af33e8afa1c17f6d2ae3e77b
ShengHaozz <shenghaoong@gmail.com>
2026-08-20 10:12:29 +0800

    Update fixtures in indiv snapshot test

--- tests/unit/test_silver_cold.py
+++ tests/unit/test_silver_cold.py
@@ -11,30 +11,25 @@ from packages.silver_cold.src.silver_cold import (
 )
 
 class Test_Silver_Cold:
-    def test_transform_indiv_snapshot(self, lta_bronze_snapshot, hdb_bronze_snapshot):
+    def test_transform_indiv_snapshot(self, lta_bronze_snapshot_generator, hdb_bronze_snapshot_generator):
+
+        lta_snapshots = lta_bronze_snapshot_generator(
+            snapshot_count=1,
+            data_count=2,
+        )
+        hdb_snapshots = hdb_bronze_snapshot_generator(
+            snapshot_count=1,
+            data_count=2,
+        )
 
         ingestion_timestamp = "2026-08-11T09:00:13.432756+00:00"
 
         table = transform(
-            lta_snapshots=lta_bronze_snapshot,
-            hdb_snapshots=hdb_bronze_snapshot,
+            lta_snapshots=lta_snapshots,
+            hdb_snapshots=hdb_snapshots,
             ingestion_timestamp=ingestion_timestamp,
         )
 
+        # Schema
         assert table.schema == SILVER_PARQUET_SCHEMA
-
-        table_list = table.to_pylist()
-
-        # Validate every output row against the Pydantic model
-        snapshots = [
-            SilverColdCarparkSnapshot.model_validate(row)
-            for row in table_list
-        ]
-
-        # Specific assertions
-        snapshots.sort(key = lambda x: x.carpark_id)
-
-        snapshot1 = snapshots[0]
-
-        assert snapshot1.
 
