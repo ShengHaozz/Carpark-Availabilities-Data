ea5d119933e45da2ae5e6eb27b8eb194421ac556
ShengHaozz <shenghaoong@gmail.com>
2026-08-25 10:22:42 +0800

    Add terraform outputs for s3 bucket to inject into dbt profile via env variable

---
+++ infra/app/outputs.tf
@@ -0,0 +1,10 @@
+output "bucket_name" {
+  description = "Name of the S3 data bucket"
+  value       = aws_s3_bucket.bucket.bucket
+}
+
+output "bucket_arn" {
+  description = "ARN of the S3 data bucket"
+  value       = aws_s3_bucket.bucket.arn
+}
+
