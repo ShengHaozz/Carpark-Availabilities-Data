1d4e6d52868bb30da33e92789a6addbe8183347a
ShengHaozz <shenghaoong@gmail.com>
2026-08-26 13:03:13 +0800

    Update role policies to be more granular

--- infra/app/gold_lambda/gold_dbt.tf
+++ infra/app/gold_lambda/gold_dbt.tf
@@ -1,3 +1,6 @@
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
 resource "aws_iam_role" "lambda_role_gold_dbt" {
   name = "lambda_role_gold_dbt"
 
@@ -39,7 +42,17 @@ resource "aws_iam_role_policy" "gold_dbt_s3_policy" {
         ]
       },
       {
-        Sid    = "ReadWriteS3Objects"
+        Sid    = "ReadSilverData"
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject"
+        ]
+        Resource = [
+          "${var.s3_bucket.arn}/level=silver/*"
+        ]
+      },
+      {
+        Sid    = "ReadWriteGoldAndAthena"
         Effect = "Allow"
         Action = [
           "s3:GetObject",
@@ -47,14 +60,15 @@ resource "aws_iam_role_policy" "gold_dbt_s3_policy" {
           "s3:DeleteObject"
         ]
         Resource = [
-          "${var.s3_bucket.arn}/*"
+          "${var.s3_bucket.arn}/level=gold/*",
+          "${var.s3_bucket.arn}/athena_query_results/*"
         ]
       }
     ]
   })
 }
 
-# Athena Query Execution Policy
+# Athena Query Execution Policy (Scoped to primary workgroup)
 resource "aws_iam_role_policy" "gold_dbt_athena_policy" {
   name = "gold-dbt-athena-policy"
   role = aws_iam_role.lambda_role_gold_dbt.name
@@ -63,14 +77,23 @@ resource "aws_iam_role_policy" "gold_dbt_athena_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid    = "AthenaQueryExecution"
+        Sid    = "AthenaWorkgroupExecution"
         Effect = "Allow"
         Action = [
           "athena:StartQueryExecution",
           "athena:GetQueryExecution",
           "athena:GetQueryResults",
           "athena:StopQueryExecution",
-          "athena:GetWorkGroup",
+          "athena:GetWorkGroup"
+        ]
+        Resource = [
+          "arn:aws:athena:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:workgroup/primary"
+        ]
+      },
+      {
+        Sid    = "AthenaGlobalDiscovery"
+        Effect = "Allow"
+        Action = [
           "athena:GetDataCatalog",
           "athena:GetDatabase",
           "athena:GetTableMetadata",
@@ -83,7 +106,7 @@ resource "aws_iam_role_policy" "gold_dbt_athena_policy" {
   })
 }
 
-# Glue Data Catalog Policy
+# Glue Data Catalog Policy (Separated Read vs Write by Layer)
 resource "aws_iam_role_policy" "gold_dbt_glue_policy" {
   name = "gold-dbt-glue-policy"
   role = aws_iam_role.lambda_role_gold_dbt.name
@@ -92,23 +115,49 @@ resource "aws_iam_role_policy" "gold_dbt_glue_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid    = "GlueCatalogAccess"
+        Sid    = "GlueCatalogDiscovery"
         Effect = "Allow"
         Action = [
-          "glue:GetDatabase",
           "glue:GetDatabases",
+          "glue:GetDatabase"
+        ]
+        Resource = [
+          "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:catalog",
+          "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:database/silver",
+          "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:database/gold"
+        ]
+      },
+      {
+        Sid    = "GlueSilverReadAccess"
+        Effect = "Allow"
+        Action = [
           "glue:GetTable",
           "glue:GetTables",
           "glue:GetPartition",
           "glue:GetPartitions",
+          "glue:BatchGetPartition"
+        ]
+        Resource = [
+          "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/silver/*"
+        ]
+      },
+      {
+        Sid    = "GlueGoldTableManagement"
+        Effect = "Allow"
+        Action = [
+          "glue:GetTable",
+          "glue:GetTables",
           "glue:CreateTable",
           "glue:UpdateTable",
           "glue:DeleteTable",
+          "glue:GetPartition",
+          "glue:GetPartitions",
           "glue:BatchCreatePartition",
-          "glue:BatchGetPartition",
           "glue:BatchDeletePartition"
         ]
-        Resource = "*"
+        Resource = [
+          "arn:aws:glue:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/gold/*"
+        ]
       }
     ]
   })
@@ -132,4 +181,3 @@ resource "aws_lambda_function" "gold_dbt_lambda" {
     }
   }
 }
-
